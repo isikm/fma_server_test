922e61fee59a86c112cf13a3cb712741428e241e
negotoro <gumusel.engin@gmail.com>
2013-12-12 22:45:13 +0100

    initial eclipse project

---
+++ Eclipse/TestWS/src/main/java/com/alphan/tryout/testws/dao/PersonDao.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alphan.tryout.testws.dao;
+
+import com.alphan.tryout.testws.model.Person;
+import com.alphan.tryout.testws.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author Alphan
+ */
+public class PersonDao {
+    
+    SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    
+    public Person getPersonById(int id) {
+        Person person = null;
+        Session session = null;
+        
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            person = (Person) session.createQuery("from Person p where p.id = :ID").setParameter("ID", id).uniqueResult();
+            session.getTransaction().commit();
+        } catch(Exception ex) {
+            
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            
+        } finally {
+            
+            if (session != null) {
+                session.close();
+            }
+            
+        }
+        
+        return person;
+    }
+    
+    public List<Person> getAllPersons() {
+        List<Person> persons = null;
+        Session session = null;
+        
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            persons = session.createQuery("from Person p").list();
+            session.getTransaction().commit();
+        } catch(Exception ex) {
+            
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            
+        } finally {
+            
+            if (session != null) {
+                session.close();
+            }
+            
+        }
+        
+        return persons;
+    }
+    
+    // This method can both insert a new person and update existing one
+    public boolean savePerson(Person person) {
+        
+        Session session = null;
+        boolean hasErrors = false;
+        
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.saveOrUpdate(person);
+            session.getTransaction().commit();
+            
+        } catch (Exception ex){
+            
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+            hasErrors = true;
+            
+        } finally {
+            
+            if (session != null) {
+                session.close();
+            }
+            
+        }       
+        
+        return !hasErrors;
+        
+    }
+}
